fd9e8b5ba47d6e01d91d03610dd9549940c37590
Sharvari134 <sharvarikhamkar455@gmail.com>
2025-01-09 00:50:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -161,6 +161,12 @@ resource "aws_security_group" "cassandra_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
 
   ingress {
     from_port   = 80
